e8bc47b9e53394ef2161de631408c1e4b94a1c37
Azed96 <Azed96>
2020-04-14 17:14:38 +0200

    Mouvement des requettes avec la souris

--- js/game.js
+++ js/game.js
@@ -71,6 +71,7 @@ var game = {
     this.displayPlayers();
 
     this.initKeyboard(game.control.onKeyDown, game.control.onKeyUp);
+    this.initMouse(game.control.onMouseMove);
 
   },
 
@@ -104,9 +105,23 @@ var game = {
   },
 
   movePlayers : function() {
-    if (game.playerOne.goUp && game.playerOne.posY > 0)
-      game.playerOne.posY-=5;
-    else if (game.playerOne.goDown && game.playerOne.posY < game.groundHeight - game.playerOne.height)
-      game.playerOne.posY+=5;
+    if ( game.control.controlSystem == "KEYBOARD" ) {
+      // keyboard control
+      if ( game.playerOne.goUp ) {
+        game.playerOne.posY-=5;
+      } else if ( game.playerOne.goDown ) {
+        game.playerOne.posY+=5;
+      }
+    } else if ( game.control.controlSystem == "MOUSE" ) {
+      // mouse control
+      if (game.playerOne.goUp && game.playerOne.posY > game.control.mousePointer)
+        game.playerOne.posY-=5;
+      else if (game.playerOne.goDown && game.playerOne.posY < game.control.mousePointer)
+        game.playerOne.posY+=5;
+    }
   },
+
+  initMouse : function(onMouseMoveFunction) {
+    window.onmousemove = onMouseMoveFunction;
+  }
 };
